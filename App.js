274a11b61b35076969d1b236aa0705bab39dd823
manishapanwar03 <114734876+manishapanwar03@users.noreply.github.com>
2023-06-04 18:47:47 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import "./App.css";
+import Sidebar from './Sidebar';
+function App () {
+  return (
+    //BEM naming convention
+    <div className='app'>
+      <div className='app_body'>
+        <Sidebar/>
+      
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCGssAMP_FMNCXVnfjc_LLZVUpqwQIHZvY",
+  authDomain: "whats-app-clone-3ad92.firebaseapp.com",
+  projectId: "whats-app-clone-3ad92",
+  storageBucket: "whats-app-clone-3ad92.appspot.com",
+  messagingSenderId: "56057173149",
+  appId: "1:56057173149:web:9285daf05e7d1e203d7074",
+  measurementId: "G-EJ86YBZY7M"
+};
